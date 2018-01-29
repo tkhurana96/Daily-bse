797fea73f44933d6aeb6e936a3ee39b87c8eeb8d
tushar khurana <tusharkhurana99@gmail.com>
2018-01-29 12:53:54 +0530

    Add fallback file to download in the downloader

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ class Downloader:
             os.mkdir(self.out_dir)
         self.fileToBeDownloaded = "EQ" + \
             self.date.strftime('%d%m%y') + "_CSV.ZIP"
-        # self.fileToBeDownloaded = "EQ220118_CSV.ZIP"
+        self.fallBackFileToBeDownloaded = "EQ240118_CSV.ZIP"
         self.fileToExtract = self.fileToBeDownloaded.split('_')[0] + ".CSV"
         self.csvFile = join(self.out_dir, self.fileToExtract)
         self.redis_db = redis_db
@@ -26,17 +26,19 @@ class Downloader:
     def StoreData(self):
         if self.redis_db is not None:
             self.redis_db.flushall()
-            with open(self.csvFile) as f:
-                reader = csv.DictReader(f)
+            try:
+                with open(self.csvFile) as f:
+                    reader = csv.DictReader(f)
 
-                for row in reader:
-                    d = dict({'code': row['SC_CODE'], 'open': row['OPEN'], 'high': row[
-                        'HIGH'], 'low': row['LOW'], 'close': row['CLOSE']})
+                    for row in reader:
+                        d = dict({'code': row['SC_CODE'], 'open': row['OPEN'], 'high': row[
+                            'HIGH'], 'low': row['LOW'], 'close': row['CLOSE']})
 
-                    self.redis_db.hmset(row['SC_NAME'].strip(), d)
+                        self.redis_db.hmset(row['SC_NAME'].strip(), d)
 
-                self.redis_db.save()
-            # print(self.redis_db.get('HDFC'))
+                    self.redis_db.save()
+            except:
+                print("Error in saving Data to redis")
 
     def GetData(self):
 
@@ -53,8 +55,23 @@ class Downloader:
                 ZipFile(f).extract(self.fileToExtract, self.out_dir)
             return True
         else:
-            print("Error in fetching file")
-            return False
+            self.fileToBeDownloaded = self.fallBackFileToBeDownloaded
+            self.fileToExtract = self.fileToBeDownloaded.split('_')[0] + ".CSV"
+            self.csvFile = join(self.out_dir, self.fileToExtract)
+
+            print(
+                "Error in fetching file, using fallback file url for 24th Jan 2018")
+            r = requests.get(
+                'http://www.bseindia.com/download/BhavCopy/Equity/' + self.fileToBeDownloaded)
+            if r.status_code == requests.codes.ok:
+                print("Creating zip file: ", self.fileToBeDownloaded)
+
+                with open(join(self.out_dir, self.fileToBeDownloaded), 'wb+') as f:
+                    f.write(r.content)
+                    ZipFile(f).extract(self.fileToExtract, self.out_dir)
+                return True
+            else:
+                return False
 
 if __name__ == "__main__":
     r = redis.StrictRedis().from_url(
@@ -62,7 +79,7 @@ if __name__ == "__main__":
     d = Downloader(
         out_dir="data", redis_db=r)
     if d.GetData():
-        d.StoreData()
         print("Fetched data successfully")
+        d.StoreData()
     else:
         print("!! Error in fetching data !!")
